72d14c452fd1e20e5feaa85395625212918560ac
linh221196 <luu.khanh.linh.1034@gmail.com>
2025-03-12 21:57:07 +0900

    rate component final touch

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -12,7 +12,7 @@ const MovieCard = ({movies}:Props) => {
         <FlatList<Movie> data={movies}
                          keyExtractor={(item) => item.id.toString()}
                          renderItem={({ item }) =>(
-                             <View className={"w-1/3 p-1 "}>
+                             <View className={"w-1/3 p-2 "}>
 
                                  <Link href={`/movies/${item.id}`}>
                                      <TouchableOpacity className={'w-full'} >
@@ -21,11 +21,15 @@ const MovieCard = ({movies}:Props) => {
                                                 'https://placehold.co/600x400/1a1a1a/ffffff.png'}}
                                         className={'w-full h-40 rounded-lg'}
                                         resizeMode={'cover'}/>
-                                         <Text className={'text-sm font-bold text-white mt-1 text-center'} numberOfLines={1}>{item.title}</Text>
-                                        <View className={'flex-row items-center justify-start gap-x-1'}>
-                                            <Image source={icons.star} className={'size-4'}/>
-                                            <Text className={'text-white'}>{Math.round(item.vote_average/2)}</Text>
+                                         <Text className={'text-sm font-bold text-white mt-1'} numberOfLines={1}>{item.title}</Text>
+                                        <View className={'flex-row items-center justify-between gap-x-1 '}>
+                                            <View className={'flex-row items-center justify-start gap-x-1'}>
+                                                <Image source={icons.star} className={'size-4'}/>
+                                                <Text className={'text-white text-xs'}>{Math.round(item.vote_average/2)}</Text>
+                                            </View>
+                                            <Text className={'text-light-300 text-xs'}>{item.release_date?.split('-')[0]}</Text>
                                         </View>
+
                                      </TouchableOpacity>
                                  </Link>
 
