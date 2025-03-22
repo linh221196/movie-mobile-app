f21a77a0ed90b68815bf95f9b1afa8de1b3e87b0
linh221196 <luu.khanh.linh.1034@gmail.com>
2025-03-23 03:30:22 +0900

    ranking added and still ok

--- components/TrendCard.tsx
+++ components/TrendCard.tsx
@@ -13,27 +13,23 @@ const TrendCard = ({movies}:Props) => {
     return (
         <FlatList<Movie> data={movies}
                          keyExtractor={(item) => item.id.toString()}
-                         renderItem={({ item }) =>(
+                         renderItem={({ item,index }) =>(
                              <View className={"w-32 p-1"}>
 
-                                 <Link href={`/movies/${item.id}`}>
+
                                      <TouchableOpacity className={'w-full'} >
                                          <Image source={{
                                              uri: item.poster_path? `https://image.tmdb.org/t/p/w500${item.poster_path}` :
                                                  'https://placehold.co/600x400/1a1a1a/ffffff.png'}}
                                                 className={'w-full h-40 rounded-lg'}
                                                 resizeMode={'cover'}/>
-                                         <Text className={'text-sm font-bold text-white mt-1'} numberOfLines={1}>{item.title}</Text>
-                                         {/*<View className={'flex-row items-center justify-between gap-x-1 '}>*/}
-                                         {/*    <View className={'flex-row items-center justify-start gap-x-1'}>*/}
-                                         {/*        <Image source={icons.star} className={'size-4'}/>*/}
-                                         {/*        <Text className={'text-white text-xs'}>{Math.round(item.vote_average/2)}</Text>*/}
-                                         {/*    </View>*/}
-                                         {/*    <Text className={'text-light-300 text-xs'}>{item.release_date?.split('-')[0]}</Text>*/}
-                                         {/*</View>*/}
+                                         <View className={'absolute bottom-2 -left-2.5 rounded-xl text-center '}>
+                                             <Text className={'text-white font-bold text-6xl bg-black/30 rounded-full'}>{index+1}</Text>
+                                         </View>
 
                                      </TouchableOpacity>
-                                 </Link>
+                                     <Text className={'text-sm font-bold text-white mt-1'} numberOfLines={1}>{item.title}</Text>
+
 
 
                              </View>
